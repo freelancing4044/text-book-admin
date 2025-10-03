795ed21c051fcd02d712ea0336ebc85d0caf4848
freelancing4044 <freelancing4044@gmail.com>
2025-10-04 02:24:07 +0530

    deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base configuration
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://text-book-backend.onrender.com/api';
 
 const http = axios.create({
   baseURL: API_BASE_URL,
